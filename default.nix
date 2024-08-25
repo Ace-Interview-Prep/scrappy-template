c606758906a5adc1654525bde0eca0d120e8fe0b
lazyLambda <galen.sprout@gmail.com>
2024-08-25 13:12:45 -0400

    scrappy-core as optional arg

--- default.nix
+++ default.nix
@@ -1,15 +1,13 @@
 { mkDerivation, aeson, base, bytestring, containers, lens, lib
 , modern-uri, network-uri, parsec, parser-combinators, text
 , transformers, pkgs
-}:
-let
-  scrappy-core = pkgs.fetchFromGitHub {
+, scrappy-core ? pkgs.haskellPackage.callPackage (pkgs.fetchFromGitHub {
     owner = "Ace-Interview-Prep";
     repo = "scrappy-core";
-    rev = "656be0c26ba86dc783f1ddda809be0d97b848efa";
-    sha256 = "sha256-1Ktja/xwStOhPIhJnV1o+Vf/u+49JJ/ysUL2aEmC20g=";
-  };
-in 
+    rev = "913670f2f83cabb2b56302e17604ec488e89da7b";
+    sha256 = "sha256-qA6+5Lxsw5q0DWYNx0VnvCmYo2f2/oKvDPVH0ZNgfXc=";
+}) {}
+}:
 mkDerivation {
   pname = "scrappy-template";
   version = "0.0.1";
@@ -17,7 +15,6 @@ mkDerivation {
   libraryHaskellDepends = [
     aeson base bytestring containers lens modern-uri network-uri parsec
     parser-combinators text transformers scrappy-core
-    (pkgs.haskellPackages.callPackage scrappy-core {})
   ];
   homepage = "https://github.com/Ace-Interview-Prep/scrappy";
   description = "html pattern matching library and high-level interface concurrent requests lib for webscraping";
