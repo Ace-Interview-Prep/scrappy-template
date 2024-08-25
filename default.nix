3da74ab99ef2ad69c6345f971e236415a9c71d57
lazyLambda <galen.sprout@gmail.com>
2024-08-24 21:44:58 -0400

    add scrappy in nix

--- default.nix
+++ default.nix
@@ -3,12 +3,12 @@
 , transformers
 }:
 mkDerivation {
-  pname = "scrappy";
-  version = "0.1.0.4";
+  pname = "scrappy-template";
+  version = "0.0.1";
   src = ./.;
   libraryHaskellDepends = [
     aeson base bytestring containers lens modern-uri network-uri parsec
-    parser-combinators text transformers
+    parser-combinators text transformers scrappy-core
   ];
   homepage = "https://github.com/Ace-Interview-Prep/scrappy";
   description = "html pattern matching library and high-level interface concurrent requests lib for webscraping";
